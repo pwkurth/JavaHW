60fbabca9974831df01bb6ebcfd7c735f8646dbf
Patrick Kurth <patrickwkurth@gmail.com>
2018-10-31 20:04:31 -0500

    Update Electronics.java

--- src/Electronics.java
+++ src/Electronics.java
@@ -4,14 +4,15 @@ public class Electronics extends Item {
     private String model; //String model
     private int year; //Initialize variable year
 
-    public void setModel(String model) {
-        this.model = model;
+    public void setModel(String value) {
+        model = value;
     }
 
     public void setYear(int year) {
         //Considered an invalid input and ignored if year<0
-        if(year >=0)
+        if (year >= 0) {
             this.year = year;
+        }
     }
 
     public String getModel() {
@@ -24,17 +25,23 @@ public class Electronics extends Item {
 
     public String formattedOutput()
     {
-        return name + "," + model + "," + year;
+        return name + "," + price + "," + model + "," + year;
     }
-//Electronics constructor with parameters
+    //Electronics constructor with parameters
     Electronics(String n, double p, String md, int my) {
 
-        if (my < 0)
+        super(n, p);
+        setYear(my);
+        model = md;
+
+        if (my < 0) {
 
             year = 0;
+        }
     }
-//Default constructor definition
-     Electronics(){
+    //Default constructor definition
+    Electronics(){
+        super();
         model = "";
         year = 0;
 
